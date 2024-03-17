f61ed3502c60e781739dc5f055657ffe1e979e42
TheFlash2k <alitaqi2000@gmail.com>
2024-03-17 09:18:01 +0500

    Added Dockerfiles for 18.04 and 20.04 setups. Also added switching in pwn-dev script

--- pwn-dev/build.sh
+++ pwn-dev/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker build -t theflash2k/pwn-dev .
+# docker build -t theflash2k/pwn-dev:22.04 .
+# docker build -t theflash2k/pwnd-dev:18.04 -f Dockerfile.1804 .
+docker build -t theflash2k/pwnd-dev:20.04 -f Dockerfile.2004 .
